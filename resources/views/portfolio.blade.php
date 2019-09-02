4621265435cb37ecf4b84c0c363020510e7bbf17
sonypiay <sonypiay@mail.com>
2019-09-02 22:36:58 +0700

    master

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -104,9 +104,9 @@
         </div>
         <div class="uk-width-1-2@xl uk-width-1-2@l uk-width-1-2@m uk-width-1-1@s">
           <div class="uk-border-rounded uk-box-shadow-large" uk-lightbox uk-scrollspy="cls: uk-animation-slide-top; delay: 200; repeat: false">
-            <a href="{{ asset('images/portfolio/gardenbuana/1.png') }}" data-caption="BiznetWifi Portal"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/gardenbuana/1.png') }});"></div></a>
+            <a href="{{ asset('images/portfolio/gardenbuana/1.png') }}" data-caption="Garden Buana"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/gardenbuana/1.png') }});"></div></a>
             @for( $i = 2; $i < 16; $i++ )
-            <a hidden href="{{ asset('images/portfolio/gardenbuana/' . $i . '.png') }}" data-caption="BiznetWifi Portal"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/gardenbuana/' . $i . '.png') }});"></div></a>
+            <a hidden href="{{ asset('images/portfolio/gardenbuana/' . $i . '.png') }}" data-caption="Garden Buana"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/gardenbuana/' . $i . '.png') }});"></div></a>
             @endfor
           </div>
         </div>
@@ -119,9 +119,9 @@
       <div class="uk-grid-medium uk-grid-match" uk-grid>
         <div class="uk-width-1-2@xl uk-width-1-2@l uk-width-1-2@m uk-width-1-1@s">
           <div class="uk-border-rounded uk-box-shadow-large" uk-lightbox uk-scrollspy="cls: uk-animation-slide-top; delay: 200; repeat: false">
-            <a href="{{ asset('images/portfolio/biznetanalytic/1.png') }}" data-caption="Project Management Tools"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/biznetanalytic/1.png') }});"></div></a>
+            <a href="{{ asset('images/portfolio/biznetanalytic/1.png') }}" data-caption="BiznetWifi Analytic"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/biznetanalytic/1.png') }});"></div></a>
             @for( $i = 2; $i <= 7; $i++ )
-            <a hidden href="{{ asset('images/portfolio/biznetanalytic/' . $i . '.png') }}" data-caption="Project Management Tools"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/biznetanalytic/' . $i . '.png') }});"></div></a>
+            <a hidden href="{{ asset('images/portfolio/biznetanalytic/' . $i . '.png') }}" data-caption="BiznetWifi Analytic"><div class="uk-background-cover uk-background-top-center uk-height-large uk-width-1-1" style="background-image: url({{ asset('images/portfolio/biznetanalytic/' . $i . '.png') }});"></div></a>
             @endfor
           </div>
         </div>
