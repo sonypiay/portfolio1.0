388cb590b0a616147b6880e03655c6acca9f6468
sonypiay <sonypiay@mail.com>
2019-09-10 23:12:52 +0700

    master

--- resources/views/inc/meta-item.blade.php
+++ resources/views/inc/meta-item.blade.php
@@ -4,10 +4,20 @@
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="application-name" content="sonypiay.com">
 <meta name="description" property="description" content="sonypiay.com" />
+<meta name="keywords" property="keywords" content="sonypiay, personal website, portfolio" />
+<meta name="google-site-verification" content="x7m2kWCsIAscUinlEm62B4lmiJWqZ68cEV917LLt2F8" />
 <link rel="canonical" href="{{ url('/') }}">
 <link rel="amphtml" href="{{ url('/') }}" />
-<meta name="keywords" property="keywords" content="sonypiay, personal website, portfolio" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.8/css/uikit.min.css" media="screen" />
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.8/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.8/js/uikit-icons.min.js"></script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-116128943-1', 'auto');
+ga('send', 'pageview')
+</script>
